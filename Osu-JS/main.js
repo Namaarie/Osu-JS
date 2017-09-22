213d850c359ac149c7bc069de4258d789ac54517
Namaarie <90soul09@gmail.com>
2017-09-22 11:56:43 -0600

    d

--- Osu-JS/main.js
+++ Osu-JS/main.js
@@ -1,12 +1,8 @@
-// P5.JS TEMPLATE
 
-// SETUP FUNCTION - Runs once at beginning of program
 function setup() {
 	createCanvas(800, 600);
 	background("white");
 }
-
-// DRAW FUNCTION - Loops @ 60FPS by default
 function draw() {
     console.log("ds");
 		console.log("tes");
