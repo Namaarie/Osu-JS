62f6064203447fd984e7a3bb952ca8aa764afdbc
Namaarie <90soul09@gmail.com>
2017-09-22 19:46:45 -0600

    quick tests

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
-
+var note;
 
 function setup() {
 	createCanvas(800, 600);
+	note = new Note();
 }
 
 function draw() {
 	background("white");
+	noStroke();
+	fill(note.color);
+	ellipse(note.x,note.y,note.w,note.h);
+
+	if(dist(mouseX,mouseY,note.x,note,y) > 25){
+		note.w = 50;
+		note.h = 50;
+	}
+}
+
+//Note Object
+function Note(){
+	this.x = width/2;
+	this.y = height/2;
+	this.w = 25;
+	this.h = 25;
+	this.color = color(255,125,125);
 }
