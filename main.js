e8f8ee4f1d2dbce25749aa6aefdd072d4d8ab191
Namaarie <90soul09@gmail.com>
2017-09-26 12:08:13 -0600

    changes

--- main.js
+++ main.js
@@ -1,37 +1,56 @@
 //Objects
 var noteSprite;
+var noteRing;
 var cursor;
+var approachCircle;
 var allNotes = [];
 
 //values
 var cursorSize;
 
+var noteCreation;
+
 function preload(){
 	//Loads images;
 	noteSprite = loadImage("assets/hitcircle.png");
+	noteRing = loadImage("assets/hitcirclering.png")
+	approachCircle = loadImage("assets/approachCircle.png");
 	cursor = loadImage("assets/cursor.png");
 }
+
 function setup() {
-	createCanvas(1280, 720);
+	createCanvas(800, 800);
 	frameRate(120);
 	noCursor();
 	cursorSize = 100;
+	noteCreation = 10;
 
-	//Creates 10 notes
-	for(i = 0; i < 10; i++){
+	//Creates notes
+	for(i = 0; i < 1; i++){
 		allNotes.push(new Note());
 	}
 }
 
 function draw() {
-	background("white");
+	background(50);
 	//Updates all current notes
 	for(i = 0; i < allNotes.length; i++){
 		allNotes[i].Render();
 		allNotes[i].Update();
+		if(allNotes[i+1] != null){
+			line(allNotes[i].x, allNotes[i].y, allNotes[i+1].x, allNotes[i+1].y);
+		}
 	}
 	//Updates cursor
 	UpdateCursor();
+
+	if(noteCreation >= 10){
+		for(i = 0; i < 1; i++){
+			noteCreation = 0;
+			allNotes.push(new Note());
+		}
+	}
+	noteCreation++;
 }
 
 //Changes cursor to custom cursor
@@ -45,8 +64,9 @@ function UpdateCursor(){
 function Note(){
 	this.x = random(width);
 	this.y = random(height);
-	this.w = 25;
-	this.h = 25;
+	this.w = 100;
+	this.h = 100;
+	this.approachCircle = 200;
 	this.color = color(random(255),random(255),random(255));
 	this.beenClicked = false;
 
@@ -54,17 +74,23 @@ function Note(){
 	this.Render = function(){
 		//Changes image anchor to center
 		imageMode(CENTER);
-		//Tints note color
-		tint(this.color);
 		//Draws the note
 		if(this.beenClicked == false){
+			//Tints note color
+			tint(this.color);
 			image(noteSprite, this.x,this.y,this.w,this.h);
+			noTint();
+			image(noteRing, this.x,this.y,this.w + 0.5,this.h + 0.5);
+			image(approachCircle, this.x, this.y, this.approachCircle, this.approachCircle);
 		}
 	}
 
 	//Updates 60 times per second
 	this.Update = function(){
-
+		this.approachCircle --;
+		if(this.approachCircle <= this.w){
+			this.Hit();
+		}
 	}
 
 	//Check if mouse is over circle
@@ -81,10 +107,32 @@ function Note(){
 		this.beenClicked = true;
 		for(i = 0; i < allNotes.length; i++){
 			if(allNotes[i].beenClicked == true){
-				allNotes[i] = new Note();
+				this.HitAccuracy();
+				//allNotes[i] = new Note();
 			}
 		}
 	}
+
+	this.HitAccuracy = function(){
+		if(this.approachCircle <= 110 && this.approachCircle > 100){
+			//300
+			console.log("300");
+			return;
+		}else if(this.approachCircle <= 130 && this.approachCircle > 110){
+			//100
+			console.log("100");
+			return;
+		}else if(this.approachCircle <= 150 && this.approachCircle > 130){
+			//50
+			console.log("50");
+			return;
+		}else{
+			//Miss
+			console.log("Miss");
+			return;
+		}
+	}
+
 }
 
 function mousePressed(){
